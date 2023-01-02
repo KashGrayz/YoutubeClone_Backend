b415a919a1677ce7cb8e631955e1bd746a72309c
emilytwines <emdelbridge@gmail.com>
2023-01-02 14:48:03 -0500

    updated requests in postman to include new api key

--- backend/replies/serializer.py
+++ backend/replies/serializer.py
@@ -5,11 +5,9 @@ class ReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = Replies
         fields = ['id', 'user', 'text', 'comment_id']
-        fields = ['id', 'user', 'comment_id', 'text']
         depth = 1
 
     comment_id = serializers.IntegerField(write_only = True)
-        comment_id = serializers.IntegerField(write_only = True)
         
 
 
